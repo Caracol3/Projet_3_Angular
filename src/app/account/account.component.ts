232b119a777f27eeb6bc6d5d702724a61dfc49f7
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-08-15 18:46:06 +0200

    new input added on the sigin form

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AccountComponent {
 
+  colorOptions: string[] = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white'];
+  selectedColor: string = 'red';
+
+  constructor() { }
+
+  saveSelectedColor() {
+
+  }
+
 }
