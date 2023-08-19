4b3c7ac33cd43af9cb78973de9c10c438cde2d32
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-08-20 01:02:56 +0200

    Finalisation contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -22,13 +22,16 @@ export class ContactComponent {
       this.contactEmailService.sendEmail(emailData).subscribe(
         response => {
           console.log('Email envoyé avec succès!', response);
+          alert('Email envoyé avec succès!');
         },
         error => {
           console.error("Erreur d'envoi du message:", error);
+          alert("Erreur d'envoi du message:");
         }
       );
     } else {
       console.log('Veuillez remplir tous les champs requis.');
+      alert('Veuillez remplir tous les champs requis.');
     }
   }
 }
