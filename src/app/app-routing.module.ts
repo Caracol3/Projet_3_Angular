ec3f251c7638798c7cc7563a18d6028b7a7a05f2
Bensadia Omar <bensadia.omar@gmail.com>
2023-08-21 14:56:46 +0200

    ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { ContactComponent } from './contact/contact.component';
+import { ChatComponent } from './chat/chat.component';
+import { AccountComponent } from './account/account.component';
+import { TrainInfoComponent } from './train-info/train-info.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path:'login',component:LoginComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'chat', component: ChatComponent},
+  { path: 'account', component: AccountComponent},
+  { path: 'train-info', component: TrainInfoComponent},
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
