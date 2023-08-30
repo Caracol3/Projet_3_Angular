bb18871595552c89640b3728882ccba0caa5f184
Bensadia Omar <bensadia.omar@gmail.com>
2023-08-30 15:11:12 +0200

    ajout du token au login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Profil } from '../models/login';
+import { tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -10,15 +13,53 @@ export class LoginComponent {
   profil: Profil = new Profil('', '', '');
   isLoginFormVisible: boolean;
 
-  constructor() {
+  constructor( private http: HttpClient) {
     this.isLoginFormVisible = false;
   }
 
+
+  // onSubmit() {
+  //   console.log('formulaire envoyé');
+  //   console.log(this.profil.email);
+  //   console.log(this.profil.password);
+  // }
+
+
   onSubmit() {
-    console.log('formulaire envoyé');
-    console.log(this.profil.email);
-    console.log(this.profil.password);
+  
+    fetch ('http://localhost:8080/login', {
+  
+      method: 'POST',
+  
+      headers: {'Content-Type': 'application/json'},
+  
+      body: JSON.stringify({
+ 
+
+        password: this.profil.password,
+
+        email: this.profil.email,
+
+  
+      })
+    })
+  
+  .then(response => response.json())
+  
+  .then(user => {
+  
+
+    console.log('user:', user);
+    if (user && user.data.token){
+            localStorage.setItem('token', user.data.token);
+    }
+    
+  
+  });
   }
+
+
+
 
   logIn() {
     console.log('login');
