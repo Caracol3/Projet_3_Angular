389712e3f5cb8ae9be8f2a9d339553ef7ef8bceb
Bensadia Omar <bensadia.omar@gmail.com>
2023-08-31 15:07:13 +0200

    api_call

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,4 +6,27 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   constructor() { }
+
+
+  // Récupération des gares de la SNCF et code uic
+
+  public async getRegions(): Promise<any> {
+    const reponse = await fetch("https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&q=&rows=3500");
+    const regionsData : {
+      records : {
+        fields : Fields
+      }[]
+    } = await reponse.json();
+    return regionsData.records.map(obj => {
+      return {
+        gare_alias_libelle_noncontraint : obj.fields.gare_alias_libelle_noncontraint,
+        commune_libellemin : obj.fields.commune_libellemin,
+        uic_code : obj.fields.uic_code,
+        adresse_cp : obj.fields.adresse_cp,
+      }
+    });
+  }
+
+
+
 }
