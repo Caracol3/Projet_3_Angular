2473ffd06b3c19abeb8b9b5d76c1e77bbdfcd631
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-09-05 10:00:24 +0200

    mise en place page admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs/operators';
 
 
 @Component({
@@ -18,11 +19,37 @@ export class AdminComponent implements OnInit {
       .subscribe(users => {
         this.users = users;
         for(let i=0; i<this.users.length; i++){
-          console.log(this.users[i].role.type + " " + this.users[i].pseudo);
+          console.log(this.users[i].role.type + " " + this.users[i].name);
         }
 
       });
 
   }
+  onRoleChange(user: User, newRole: string) {
+    const newRoleId = newRole === 'ADMIN' ? 1 : 2; // Convertit le rôle en ID (Admin -> 1, User -> 2)
+
+    // Créez le corps de la requête JSON
+    const requestBody = {
+      id: user.id, // ID de l'utilisateur cible
+      role: {
+        id: newRoleId, // Nouvel ID de rôle
+        type: newRole // Nouveau rôle (Admin ou User)
+      }
+    };
+
+    // Ici on effectue la requête HTTP PUT
+    this.httpClient
+    .put(`http://localhost:8080/admin/users/${user.id}/role`, requestBody)
+    .pipe(take(1))
+    .subscribe(
+      () => {
+        console.log('Rôle mis à jour avec succès !');
+      },
+      (error) => {
+        console.error('Erreur dans la mise à jour du rôle :', error);
+      }
+    );
 }
+}
+
 
