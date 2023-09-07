b0825ecc4517437f355efa3179ff7b6d5a0b8c45
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-09-07 21:49:23 +0200

    avatar in the modal can be seen

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -27,43 +27,57 @@ export class AccountComponent implements OnInit {
   is_available: boolean = true;
   isModalOpen: boolean = false;
   avatarOptions: string[] = [
-    'avatar/AvatarF1.png',
-    'avatar/AvatarF2.png',
-    'avatar/AvatarF3.png',
-    'avatar/AvatarF4.png',
-    'avatar/AvatarF5.png',
-    'avatar/AvatarF6.png',
-    'avatar/AvatarF7.png',
-    'avatar/AvatarF8.png',
-    'avatar/AvatarF9.png',
-    'avatar/AvatarF10.png',
-    'avatar/AvatarF11.png',
-    'avatar/AvatarF12.png',
-    'avatar/AvatarF13.png',
-    'avatar/AvatarF14.png',
-    'avatar/AvatarF15.png',
-    'avatar/AvatarH1.png',
-    'avatar/AvatarH2.png',
-    'avatar/AvatarH3.png',
-    'avatar/AvatarH4.png',
-    'avatar/AvatarH5.png',
-    'avatar/AvatarH6.png',
-    'avatar/AvatarH7.png',
-    'avatar/AvatarH8.png',
-    'avatar/Avatar9.png',
-    'avatar/AvatarH10.png',
-    'avatar/AvatarH11.png',
-    'avatar/AvatarH12.png',
-    'avatar/AvatarH13.png',
-    'avatar/AvatarH14.png',
-    'avatar/AvatarH15.png',
-    'avatar/AvatarH16.png',
-    'avatar/AvatarH17.png',
-    'avatar/AvatarH18.png',
+    '/assets/avatar/AvatarF1.png',
+    '/assets//assets/avatar/AvatarF2.png',
+    '/assets/avatar/AvatarF3.png',
+    '/assets/avatar/AvatarF4.png',
+    '/assets/avatar/AvatarF5.png',
+    '/assets/avatar/AvatarF6.png',
+    '/assets/avatar/AvatarF7.png',
+    '/assets/avatar/AvatarF8.png',
+    '/assets/avatar/AvatarF9.png',
+    '/assets/avatar/AvatarF10.png',
+    '/assets/avatar/AvatarF11.png',
+    '/assets/avatar/AvatarF12.png',
+    '/assets/avatar/AvatarF13.png',
+    '/assets/avatar/AvatarF14.png',
+    '/assets/avatar/AvatarF15.png',
+    '/assets/avatar/AvatarH1.png',
+    '/assets/avatar/AvatarH2.png',
+    '/assets/avatar/AvatarH3.png',
+    '/assets/avatar/AvatarH4.png',
+    '/assets/avatar/AvatarH5.png',
+    '/assets/avatar/AvatarH6.png',
+    '/assets/avatar/AvatarH7.png',
+    '/assets/avatar/AvatarH8.png',
+    '/assets/avatar/Avatar9.png',
+    '/assets/avatar/AvatarH10.png',
+    '/assets/avatar/AvatarH11.png',
+    '/assets/avatar/AvatarH12.png',
+    '/assets/avatar/AvatarH13.png',
+    '/assets/avatar/AvatarH14.png',
+    '/assets/avatar/AvatarH15.png',
+    '/assets/avatar/AvatarH16.png',
+    '/assets/avatar/AvatarH17.png',
+    '/assets/avatar/AvatarH18.png',
   ];
   user: any = {} ;
 
   constructor(private httpClient: HttpClient, private accountService : AccountServiceService) {}
+
+  openAvatarModal() {
+    this.isModalOpen = true;
+  }
+
+  selectAvatar(avatar: string) {
+    this.user.avatar = avatar; // Met à jour l'avatar de l'utilisateur
+    this.isModalOpen = false; // Ferme la modale
+  }
+
+  closeAvatarModal() {
+    this.isModalOpen = false; // Fermer la modale sans sélection
+  }
+
 
   ngOnInit(): void {
     console.log(this.accountService.userId);
