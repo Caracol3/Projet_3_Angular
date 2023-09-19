acf2ee59b8e88f47a8b0b71018cd94c28f0894ca
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-09-19 11:32:11 +0200

    added avatar model

---
+++ src/app/models/avatar.ts
@@ -0,0 +1,7 @@
+export class avatar
+{
+  avatarList: string[] = [
+  "src/assets/Avatar/alien.png",
+   src/assets/Avatar/arbre.png src/assets/Avatar/AvatarF1.png src/assets/Avatar/AvatarF4.png src/assets/Avatar/AvatarF6.png src/assets/Avatar/AvatarF8.png src/assets/Avatar/AvatarF9.png src/assets/Avatar/AvatarF11.png src/assets/Avatar/AvatarF13.png src/assets/Avatar/AvatarF15.png src/assets/Avatar/AvatarH1.png src/assets/Avatar/AvatarH3.png src/assets/Avatar/AvatarH4.png src/assets/Avatar/AvatarH7.png src/assets/Avatar/AvatarH10.png src/assets/Avatar/AvatarH11.png src/assets/Avatar/AvatarH13.png src/assets/Avatar/AvatarH14.png src/assets/Avatar/bff.png src/assets/Avatar/chat-noir.png src/assets/Avatar/chat.png src/assets/Avatar/chevalier.png src/assets/Avatar/chien1.png src/assets/Avatar/chien2.png src/assets/Avatar/dragon.png src/assets/Avatar/hommefuture.png src/assets/Avatar/lapin.png src/assets/Avatar/licorne1.png src/assets/Avatar/licorne2.png src/assets/Avatar/oiseau.png src/assets/Avatar/princesse.png src/assets/Avatar/roi.png src/assets/Avatar/viking.png
+  ];
+}
