27973ba52e2ce4a50444291fb42e61e5034a7e6b
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-09-25 10:58:34 +0200

    textarea fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'projet3';
 
+constructor(private router:Router) {}
 
+isRouteChatOpen(): boolean {
+  return this.router.url === '/chat';
+}
 }
